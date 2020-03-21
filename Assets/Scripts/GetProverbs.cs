bc25edef9fb7a19b67cedc265989f5b89b9dc40f
Groszekk <groszekoriginal@gmail.com>
2020-03-21 23:22:49 +0100

    basic debuglog proverbs

---
+++ Assets/Scripts/GetProverbs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetProverbs : MonoBehaviour
+{
+    private string proverbs = Pickup.Text;
+    public Button button;
+
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(ReadProverbs);
+    }
+
+    void ReadProverbs()
+    {
+        Debug.Log(proverbs);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float move_speed = 5f;
+
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * move_speed * Time.deltaTime, 0f, 0f));
+        }
+
+        if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * move_speed * Time.deltaTime, 0f));
+        }
+    }
+}
