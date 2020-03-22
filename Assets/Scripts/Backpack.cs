f7f373596c030e95ec70a4edacee2effc582e2c5
Groszekk <groszekoriginal@gmail.com>
2020-03-22 17:44:54 +0100

    backpack

---
+++ Assets/Scripts/Backpack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Backpack : MonoBehaviour
+{
+    public GameObject inventory;
+    public Button button;
+
+    void Start()
+    {
+        GameObject equ = inventory.GetComponent<GameObject>();
+        Button btn = button.GetComponent<Button>();
+
+        button.onClick.AddListener(ShowEqu);
+
+        inventory.SetActive(false);
+    }
+
+    void ShowEqu()
+    {
+        if(!inventory.activeInHierarchy)
+        {
+            inventory.SetActive(true);
+        }else
+        {
+            inventory.SetActive(false);
+        }
+    }
+}
