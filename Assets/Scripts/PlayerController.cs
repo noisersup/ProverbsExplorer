0dccfcba12b2f054d905f615cbe5e1d29d5fe4fa
noisersup <patekiapl@gmail.com>
2020-03-22 13:44:07 +0100

    Some fixes in Movement system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public PlayerMovement player_movement;
     public EntityDamage player_damage;
-    void Start(){
-    }
+    
     void Update(){
-        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-            player_movement.MoveH(Input.GetAxisRaw("Horizontal"));
-
-        if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-            player_movement.MoveV(Input.GetAxisRaw("Vertical"));
-
+        player_movement.Move(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float move_speed = 5f;
 
-    public void MoveH( float horizontal )
+    public void Move( float horizontal, float vertical )
     {
-        transform.Translate(new Vector3(horizontal * move_speed * Time.deltaTime, 0));
-    }
-    public void MoveV(float vertical )
-    {
-        transform.Translate(new Vector3(0, vertical * move_speed * Time.deltaTime));
+        transform.Translate(new Vector3(horizontal * move_speed * Time.deltaTime, vertical * move_speed * Time.deltaTime));
     }
 }
