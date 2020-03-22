d6e54f09be18a5c96367ca96e029a8e60dee8814
noisersup <patekiapl@gmail.com>
2020-03-22 13:13:24 +0100

    Added Camera movement
    Smooth movement with camera speed, offset, target and movement config.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject target;
+    public float distance;
+    public Vector2 offset;
+    public float camera_speed;
+    
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 target_position = new Vector3(target.transform.position.x + offset.x,
+                                              target.transform.position.y + offset.y,
+                                              transform.position.z);
+                                              
+        transform.position = Vector3.Lerp(transform.position, target_position, camera_speed);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, distance, camera_speed);
+    }
+}
