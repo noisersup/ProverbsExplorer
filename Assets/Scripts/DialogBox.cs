f5f3ca183ef12eb581001e6a053d0e8adb5629bc
noisersup <patekiapl@gmail.com>
2020-03-23 17:36:07 +0100

    Basic DialogBox
    Just Prompt(text) and close after spacebar

---
+++ Assets/Scripts/DialogBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogBox : MonoBehaviour
+{
+    public GameObject box;
+    public bool debug_box = false;
+    RectTransform rect;
+    Text text_box;
+    bool open = false;
+    void Start()
+    {
+        box.SetActive(false);
+        rect = (RectTransform) box.GetComponent("RectTransform");
+        text_box = box.transform.Find("Text").GetComponent<Text>();
+    }
+    void Update()
+    {
+        if(debug_box) Prompt("Lorem ipsum dolor sit amet, consectetur adipiscing elit. ");
+        if(Input.GetKey("space") && open) Close();
+    }
+    public void Prompt(string content)
+    {
+        open=true;
+        box.SetActive(true);
+        text_box.text = content;
+    }
+    void Close()
+    {
+        open=false;
+        box.SetActive(false);
+    }
+
+}
