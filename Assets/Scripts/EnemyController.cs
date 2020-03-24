22ac7c98103992ac533f7a827eb59da014ac0084
noisersup <patekiapl@gmail.com>
2020-03-24 12:37:10 +0100

    Fixed pushing player by enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,18 +6,22 @@ public class EnemyController : MonoBehaviour
 {
     public EntityMovement movement;
     public GameObject target;
+    public float max_range;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        FollowTarget(target);
+    {   
+        float a = (target.transform.position.x - transform.transform.position.x) * (target.transform.position.x - transform.transform.position.x);
+        float b = (target.transform.position.y - transform.transform.position.y) * (target.transform.position.y - transform.transform.position.y);
+        if(a+b >= max_range) FollowTarget(target);
     }
     void FollowTarget(GameObject target){
         Vector2 direction = (target.transform.position - transform.transform.position).normalized;
         movement.Move(direction.x,direction.y);
     }
+
 }
