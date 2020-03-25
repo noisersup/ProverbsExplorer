b0b623ca099dbf6616c01ee17fc761e96ccce197
noisersup <patekiapl@gmail.com>
2020-03-26 00:25:44 +0100

    Very basic attack from Enemy and cooldown in EntityDamage

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public EntityMovement movement;
-    public GameObject target;
-    public float max_range;
-    void Start()
+    [SerializeField] EntityMovement movement;
+    [SerializeField] EntityDamage damage_system;
+    [SerializeField] GameObject target;
+    [SerializeField] float max_range;
+
+    bool target_collided = false;
+    List <BoxCollider2D> collisions = new List <BoxCollider2D>();
+    void OnTriggerEnter2D(Collider2D other)
     {
-
+        if(other.gameObject == target) target_collided = true;  
     }
-
-    // Update is called once per frame
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject == target) target_collided = false;
+    }
     void Update()
     {   
+        if(target_collided) 
+        {
+            AttackTarget(target);
+        }
         float a = (target.transform.position.x - transform.transform.position.x) * (target.transform.position.x - transform.transform.position.x);
         float b = (target.transform.position.y - transform.transform.position.y) * (target.transform.position.y - transform.transform.position.y);
         if(a+b >= max_range) FollowTarget(target);
     }
-    void FollowTarget(GameObject target){
+    void FollowTarget(GameObject target)
+    {
         Vector2 direction = (target.transform.position - transform.transform.position).normalized;
         movement.Move(direction.x,direction.y);
     }
-
+    void AttackTarget(GameObject target)
+    {
+        damage_system.Attack(target.transform.position);
+    }
 }
--- Assets/Scripts/EntityDamage.cs
+++ Assets/Scripts/EntityDamage.cs
@@ -7,25 +7,49 @@ public class EntityDamage : MonoBehaviour
 
     [SerializeField] int max_hp=10;
     [SerializeField] int damage=5;
-    
+    [SerializeField] float cooldown=2;
+
     int hp;
-     List <BoxCollider2D> collisions = new List <BoxCollider2D>();
+    bool cooldown_active = false;
+    float cooldown_progress = 0;
+    List <BoxCollider2D> collisions = new List <BoxCollider2D>();  
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.GetType() == typeof(BoxCollider2D)) collisions.Add((BoxCollider2D) other);  
     }
-    void OnTriggerExit2D(Collider2D other){
+    void OnTriggerExit2D(Collider2D other)
+    {
         if(other.GetType() == typeof(BoxCollider2D)) collisions.Remove((BoxCollider2D) other);  
     }
     void Start()
     {
         hp = max_hp;
     }
+    void Update()
+    {
+        if(cooldown_active)
+        {
+            cooldown_progress += Time.deltaTime;
+            if(cooldown_progress >= cooldown)
+            {
+                cooldown_active = false;
+                cooldown_progress = 0;
+            }
+        }
+    }
     
     public void Attack(Vector2 location)
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(location,0.001f);
-        if(colliders.Length>0 && collisions.Contains((BoxCollider2D) colliders[0].GetComponent("BoxCollider2D")))  GiveDamage(colliders[0].gameObject);
+        if(!cooldown_active)
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(location,0.001f);
+            if(colliders.Length>0 && collisions.Contains((BoxCollider2D) colliders[0].GetComponent("BoxCollider2D")))
+            {
+                GiveDamage(colliders[0].gameObject);
+                cooldown_active = true;
+            }
+        }
     }
     public void TakeDamage(int taken_damage)
     {
