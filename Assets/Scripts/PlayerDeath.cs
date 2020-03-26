cb6458f18feed18e541c93c51b7e13d50064b3d4
noisersup <patekiapl@gmail.com>
2020-03-26 06:32:50 +0100

    scene with full map

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    EntityDamage player_damage;
+    void Start(){
+        
+    }
+    public void DeathScreen(){
+        
+    } 
+}
