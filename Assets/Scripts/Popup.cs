bc83ad0a5e119a4eb3b3c102e605d085c07d2112
Groszekk <43693079+Groszekk@users.noreply.github.com>
2020-03-26 06:16:17 +0100

    popup

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour
+{
+    public Button button;
+    public Image img;
+    public Text txt;
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(ClosePopUp);
+    }
+
+    void ClosePopUp()
+    {
+        img.enabled = false;
+        txt.enabled = false;
+    }
+}
